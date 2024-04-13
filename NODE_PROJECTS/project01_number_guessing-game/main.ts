a2a4bc09e974a7b94309e950b01dc28492d07c8c
channayousif <channayousif@yahoo.com>
2024-04-13 13:49:47 +0500

    Added shebang

--- NODE_PROJECTS/project01_number_guessing-game/main.ts
+++ NODE_PROJECTS/project01_number_guessing-game/main.ts
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 /**
  * @Date: 13-04-2024
  * @author: channayousif
@@ -12,33 +13,42 @@ import inquirer from "inquirer";
 
 let random_number = 0;
 let score = 0;
+console.log(" --------------------------------");
+console.log("|      Number Guessing Game      |");
+console.log(" --------------------------------");
+console.log('(Press 0 to quit the game)');
 while(true){
 
 //Generate random number
 random_number=Math.ceil(Math.random()*10);
-
 //take user input
 const user_input = await inquirer.prompt([{
-    message:"Please Guess a number from 0 to 10 (Press Q or q to quit the game):",
+    message:"Please Guess a number from 1 to 10 :",
     name:"number",
     type:"input"
 }])
-
 //Generate result
-if(user_input.number=="Q"||user_input.number=="q"){
+let temp =Math.abs(random_number - user_input.number);
+if(user_input.number === "0"){
     break;
-}else if(random_number-user_input.number <5){
-    score=+5;
-    console.log(`you are near! your score is ${score}.`)
-}else{
-    score=-5;
-    console.log(`you are not near! your score is ${score}.`)
+}else if(temp == 0){
+    score+=(10);
+    console.log(`*********Bulls eye!***********\nYou guessed correctly! \nRandom number was ${random_number} \nYour guess was ${user_input.number}.\n Difference was ${temp} \n****your score is now ${score}****`)
+}else if(temp <= 1){
+    score+=(7);
+    console.log(`Your guess was Very good! \nRandom number was ${random_number} \nYour guess was ${user_input.number}.\n Difference was ${temp} \n****your score is now ${score}****`)
+}else if(temp <= 3){
+    score+=(6-temp);
+    console.log(`Your guess was good! \nRandom number was ${random_number} \nYour guess was ${user_input.number}.\n Difference was ${temp} \n****your score is now ${score}****`)
+}else if(temp >= 3){
+    score-=(temp-3);
+    console.log(`Your guess was not good!!\nRandom number was ${random_number} \nYour guess was ${user_input.number}.\n Difference was ${temp} \n****your score is now ${score}****`)
 };
-
 if(score<=0){
     console.log("you loose!")
     break;
 }
+
 
 
 }
