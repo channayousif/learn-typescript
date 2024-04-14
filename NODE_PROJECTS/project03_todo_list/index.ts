ad13fe9b039068d16ab3bee2f526949de1045b45
channayousif <channayousif@yahoo.com>
2024-04-14 15:43:49 +0500

    completed

--- NODE_PROJECTS/project03_todo_list/index.ts
+++ NODE_PROJECTS/project03_todo_list/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 
 /**
  * @Date 14-04-2024
@@ -6,3 +7,43 @@
  * @description: A simple command line Todo app
  * 
  */
+
+import inquirer from "inquirer";
+let todos=[];
+console.log("------Welcome to Channayousif todo list App------")
+while(true){
+    const action= await inquirer.prompt([{
+        message:"Please select desired option:",
+        type:"list",
+        name:"choice",
+        choices:["Add items in list","view my to do list","Exit"]
+    }])
+    if (action.choice=="view my to do list"){
+        console.log("------My todo list------")
+        for(let i=0;i<todos.length;i++){
+            console.log(`${i+1}. ${todos[i]}`);
+        }
+        console.log("------End of todo list------")
+    }else if(action.choice=="Exit"){
+        break
+    }else {
+        while(true){
+        
+            const todo= await inquirer.prompt([{
+                message:"Please enter your item in todo list(Q or q to stop adding):",
+                type:"input",
+                name:"item"
+            }])
+            if(todo.item=="q"){
+                break;
+            }else if (todo.item!=""){
+                todos.push(todo.item);
+            }else {
+                console.log("Empty, try again!");
+                continue;
+            }
+            
+        }
+        
+    }
+}
